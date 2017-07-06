73595ada381fe921a32c5d5805f3ad867c84aa7d
louis giesen <lgiesen@vente-privee.com>
2017-07-06 19:35:33 +0200

    push start of the website

--- biereping/biereping.py
+++ biereping/biereping.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 
-from flask import Flask, request, abort, render_template, jsonify
+from flask import Flask, request, abort, render_template, redirect, url_for
 import sys
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+
+@app.route('/create_event', methods=['GET'])
+def create_event():
+        return render_template('create_event.html', params={'name':'michel', 'age':20})
+
+
+@app.route('/event', methods=['POST'])
+def add_event():
+        args = request.form.to_dict()
+        print args
         return render_template('index.html')
 
+@app.route('/', methods=['GET'])
+def index():
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000)
